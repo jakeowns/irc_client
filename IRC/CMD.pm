84e88e9ee50c74cb1f6485794fc6b9fd3e41b0b8
jakeowns <beanshield@gmail.com>
2013-10-15 19:10:30 -0700

    modified:   IRC/CMD.pm  -removed s///, changed to m///.

--- IRC/CMD.pm
+++ IRC/CMD.pm
@@ -6,13 +6,11 @@ use Switch::Plain;
 sub get {
     my $class = shift;
     $_ = shift;
-    s!^(.+) (.*)!!;
+    m/^(.+) (.*)/;
     my ( $cmd, $arg ) = ( $1, $2 );
     $cmd =~ tr!A-Z!a-z!;
     sswitch($cmd) {
-        case 'j' : {
-            "join $arg";
-        }
+        case 'j' : { "join $arg"; }
         case 'q' : { "quit"; }
         default  : {
             ( defined $arg ) ? "$cmd $arg" : "$cmd";
