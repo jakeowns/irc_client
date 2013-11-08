fb198cdf99cf293f04096a75522fe37a62fc9cef
jakeowns <beanshield@gmail.com>
2013-11-07 17:20:02 -0800

    Use Moose.

--- lib/IRC.pm
+++ lib/IRC.pm
@@ -1,50 +1,49 @@
+package IRC;
 use strict;
 use warnings;
-
-package IRC;
 use Socket qw(pack_sockaddr_in inet_aton);
+use Moose;
 
-sub new {
-    my ( $class, $args ) = @_;
-    my $self = bless {}, $class;
-    return $self->_init($args);
-}
-
-sub DESTROY {
-    my $self = shift;
-    close( $self->{_sock} ) if $self->{_sock};
-}
-
-sub _init {
-    my ( $self, $args ) = @_;
-    $self->{_sock}    = $args->{sock}    || die "no socket: $!";
-    $self->{_server}  = $args->{server}  || "irc.freenode.net";
-    $self->{_port}    = $args->{port}    || 6667;
-    $self->{_channel} = $args->{channel} || [];
-    $self->{_nick}    = $args->{nick}
-      || join( '', map { ( "a" .. "z" )[ rand 26 ] } 1 .. 8 );
-    return $self;
-}
+has 'sock' => ( is => 'ro', );
+has 'server' => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => 'irc.freenode.net',
+);
+has 'port' => (
+    is      => 'ro',
+    isa     => 'Int',
+    default => 6667,
+);
+has 'channel' => (
+    is  => 'rw',
+    isa => 'ArrayRef[Str]',
+);
+has 'nick' => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => join( '', map { ( "a" .. "z" )[ rand 26 ] } 1 .. 8 ),
+);
 
 sub connect {
     my $self = shift;
-    connect( $self->{_sock},
-        pack_sockaddr_in( $self->{_port}, inet_aton( $self->{_server} ) ) )
+    connect( $self->{sock},
+        pack_sockaddr_in( $self->{port}, inet_aton( $self->{server} ) ) )
       or die "connect: $!";
     $self->login;
 }
 
 sub login {
     my $self = shift;
-    my $sock = $self->{_sock};
-    send( $sock, "NICK $self->{_nick}\r\n", 0 );
-    send( $sock, "USER $self->{_nick} 8 * $self->{_nick}\r\n", 0 );
+    my $sock = $self->{sock};
+    send( $sock, "NICK $self->{nick}\r\n", 0 );
+    send( $sock, "USER $self->{nick} 8 * $self->{nick}\r\n", 0 );
 }
 
 sub join_chan {
     my $self = shift;
-    my $sock = $self->{_sock};
-    foreach my $chan ( @{ $self->{_channel} } ) {
+    my $sock = $self->{sock};
+    foreach my $chan ( @{ $self->{channel} } ) {
         send( $sock, "JOIN $chan\r\n", 0 );
     }
 
@@ -52,7 +51,7 @@ sub join_chan {
 
 sub read {
     my $self = shift;
-    my $sock = $self->{_sock};
+    my $sock = $self->{sock};
     while (<$sock>) {
         if (/^PING(.*)$/i) {
             send( $sock, "PONG $1\r\n", 0 );
@@ -66,12 +65,12 @@ sub read {
 
 sub write {
     my $self = shift;
-    my $sock = $self->{_sock};
+    my $sock = $self->{sock};
     send( $sock, $_[0], 0 );
 }
 
 sub get_nick {
     my $self = shift;
-    return $self->{_nick};
+    return $self->{nick};
 }
 1;
